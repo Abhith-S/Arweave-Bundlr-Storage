22f9737c5dc695c29452a2343ccc3a8d68415b74
Abhith Shaji <75518181+Abhith-S@users.noreply.github.com>
2022-11-11 16:34:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Arweave-Bundlr-file-storage
-Simple file storage on Arweave using Bundlr
+## Basic example of using Bundlr + Arweave in a client-side JavaScript application
+
+To run this project:
+
+1. Clone the repo
+
+```sh
+git clone git@github.com:dabit3/bundlr-arweave-example.git
+```
+
+2. Change into the directory
+
+```sh
+cd bundlr-arweave-example
+```
+
+3. Install the dependencies
+
+```sh
+npm install
+```
+
+4. Run the app (be sure to switch wallet to Matic network)
+
+```sh
+npm run dev
+```
---
+++ pages/index.js
@@ -0,0 +1,97 @@
+
+import BigNumber from 'bignumber.js'
+import { useContext, useState } from 'react'
+import { MainContext } from '../context'
+
+export default function Home() {
+  const [file, setFile] = useState()
+  const [image, setImage] = useState()
+  const [URI, setURI] = useState()
+  const [amount, setAmount] = useState()
+  const { bundlrInstance, initialiseBundlr, balance, fetchBalance } = useContext(MainContext)
+  async function initialize() {
+    initialiseBundlr()
+  }
+  function onFileChange(e) {
+    const file = e.target.files[0]
+    if (file) {
+      const image = URL.createObjectURL(file)
+      setImage(image)
+      let reader = new FileReader()
+      reader.onload = function () {
+        if (reader.result) {
+          setFile(Buffer.from(reader.result))
+        }
+      }
+      reader.readAsArrayBuffer(file)
+    }
+  }
+
+  async function uploadFile() {
+    let tx = await bundlrInstance.uploader.upload(file, [{ name: "Content-Type", value: "application/pdf" }])
+    console.log('tx: ', tx)
+    setURI(`http://arweave.net/${tx.data.id}`)
+
+  }
+
+  async function fundWallet() {
+    if (!amount) return
+    const amountParsed = parseInput(amount)
+    let response = await bundlrInstance.fund(amountParsed)
+    console.log('Wallet funded: ', response)
+    fetchBalance()
+  }
+
+  function parseInput (input) {
+    const conv = new BigNumber(input).multipliedBy(bundlrInstance.currencyConfig.base[1])
+    if (conv.isLessThan(1)) {
+      console.log('error: value too small')
+      return
+    } else {
+      return conv
+    }
+  }
+
+  return (
+
+    <div style={containerStyle}>
+    <h1>Arweave-Bundlr Test</h1>
+      {
+        !balance && <button onClick={initialize}>Connect Wallet</button>
+      }
+      {
+        balance && (
+          <div>
+            <h4>Balance (MATIC): {balance}</h4>
+            <div style={{ padding: '20px 0px'}}>
+              <input  placeholder="Enter Amount" onChange={e => setAmount(e.target.value)} />
+              <button onClick={fundWallet}>Deposit</button>
+            </div>
+            <br/>
+            <input
+              type="file"
+              onChange={onFileChange}
+            />
+            <br />
+            <br/>
+            {
+              image && <embed src={image} style={{ width: '350px', height: '450px' }} />
+            }
+            <br/>
+            <br/>
+            <button onClick={uploadFile}>Upload File</button>
+            <br/>
+            <br/>
+            {
+              URI && <a href={URI}>{URI}</a>
+            }
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+const containerStyle = {
+  // padding: '5px 20px'
+}
